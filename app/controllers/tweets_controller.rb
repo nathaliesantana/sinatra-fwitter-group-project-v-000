1484bc75b5513c820ae88951baca6ad0d6c864e2
Nathalie Santana <nathalie.scruz@gmail.com>
2019-03-13 02:05:29 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,6 +10,14 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
 
 
 end
